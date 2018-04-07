724af6d473192511b624cd4c0246b4522735e435
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-07 18:02:33 +0800

    Add bifurcate

---
+++ ThirtySecondsOfCSharp.Tests/Lists/BifurcateTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class BifurcateTest
+    {
+        [Fact]
+        public void ShouldDivideNumbersIntoTwoGroups()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var filter = new List<bool> { true, true, true, false, false, false };
+            var groups = ThirtySecondsOfCSharp.Lists.Bifurcate(numbers, filter);
+
+            Assert.Equal(new List<int> { 1, 2, 3 }, groups[0]);
+            Assert.Equal(new List<int> { 4, 5, 6 }, groups[1]);
+        }
+    }
+}
--- ThirtySecondsOfCSharp/Lists.cs
+++ ThirtySecondsOfCSharp/Lists.cs
@@ -16,6 +16,20 @@ namespace ThirtySecondsOfCSharp
             return list.Exists(predicate);
         }
 
+        public static List<List<T>> Bifurcate<T>(List<T> list, List<bool> filter)
+        {
+            return Enumerable.Range(0, list.Count)
+                .Aggregate(new List<List<T>> { new List<T>(), new List<T>() }, (accumulator, i) =>
+                {
+                    var index = filter[i] ? 0 : 1;
+
+                    accumulator[index].Add(list[index]);
+
+                    return accumulator;
+                })
+                .ToList();
+        }
+
         public static List<List<T>> BifurcateBy<T>(List<T> list, Predicate<T> predicate)
         {
             return list.Aggregate(new List<List<T>> { new List<T>(), new List<T>() }, (accumulator, current) =>
