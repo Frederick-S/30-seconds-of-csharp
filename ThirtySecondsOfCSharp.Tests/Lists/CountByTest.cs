5ed8d52e7b087f6c39892debe9b4fe74d1b810ca
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-14 21:03:09 +0800

    Add countby

---
+++ ThirtySecondsOfCSharp.Tests/Lists/CountByTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class CountByTest
+    {
+        [Fact]
+        public void ShouldGroupStringsIntoTwoGroups()
+        {
+            var strings = new List<string> { "one", "two", "three" };
+            var expectedGroups = new Dictionary<int, int>
+            {
+                { 3, 2 },
+                { 5, 1 },
+            };
+
+            Assert.Equal(expectedGroups, ThirtySecondsOfCSharp.Lists.Lists.CountBy(strings, x => x.Length));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/CountBy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static Dictionary<TResult, int> CountBy<T, TResult>(List<T> list, Func<T, TResult> keySelector)
+        {
+            return list.GroupBy(keySelector)
+                .ToDictionary(group => group.Key, group => group.Count());
+        }
+    }
+}
