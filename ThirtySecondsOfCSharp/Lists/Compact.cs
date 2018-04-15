7cb1ae3bb8b88933a3b562281c98e9799cea4fc2
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-15 09:37:04 +0800

    Add type constraint

--- ThirtySecondsOfCSharp/Lists/Compact.cs
+++ ThirtySecondsOfCSharp/Lists/Compact.cs
@@ -6,6 +6,7 @@ namespace ThirtySecondsOfCSharp.Lists
     public partial class Lists
     {
         public static List<T> Compact<T>(List<T> list)
+            where T : class
         {
             return list.FindAll(x => x != null).ToList();
         }
