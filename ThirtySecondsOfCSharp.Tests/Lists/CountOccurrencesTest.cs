0c7fef62d277956f6e76eeba7b2588866a67628a
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-15 09:26:41 +0800

    Add count occurrences

---
+++ ThirtySecondsOfCSharp.Tests/Lists/CountOccurrencesTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class CountOccurrencesTest
+    {
+        [Fact]
+        public void ShouldReturnOccurrencesAsThree()
+        {
+            var numbers = new List<int> { 1, 1, 2, 1, 2, 3 };
+
+            Assert.Equal(3, ThirtySecondsOfCSharp.Lists.Lists.CountOccurrences(numbers, 1));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/CountOccurrences.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static int CountOccurrences<T>(List<T> list, T value)
+        {
+            return list.Aggregate(0, (accumulator, current) => value.Equals(current) ? accumulator + 1 : accumulator);
+        }
+    }
+}
