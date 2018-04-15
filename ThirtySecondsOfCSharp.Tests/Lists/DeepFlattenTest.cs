13bbab8aa0305564fd49b2bd122848d4a2abea67
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-15 12:15:34 +0800

    Add deep flatten

---
+++ ThirtySecondsOfCSharp.Tests/Lists/DeepFlattenTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class DeepFlattenTest
+    {
+        [Fact]
+        public void ShouldDeepFlattenNumbers()
+        {
+            var numbers = new List<object>
+            {
+                1,
+                new List<int> { 2 },
+                new List<object>
+                {
+                    new List<int> { 3 },
+                    4,
+                },
+                5,
+            };
+            var expectedList = new List<int> { 1, 2, 3, 4, 5 };
+            var actualList = ThirtySecondsOfCSharp.Lists.Lists.DeepFlatten(numbers);
+
+            Enumerable.Range(0, actualList.Count)
+                .ToList()
+                .ForEach(i => Assert.Equal(expectedList[i], actualList[i]));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/DeepFlatten.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<object> DeepFlatten(List<object> list)
+        {
+            return list.SelectMany(x =>
+            {
+                if (x is IList)
+                {
+                    return DeepFlatten((x as IList).Cast<object>().ToList());
+                }
+                else
+                {
+                    return new List<object> { x };
+                }
+            })
+            .ToList();
+        }
+    }
+}
