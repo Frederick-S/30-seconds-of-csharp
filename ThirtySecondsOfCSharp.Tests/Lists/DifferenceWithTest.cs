36df85cf6a87c113674b9b13e68b5dcf21d93327
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-16 22:16:48 +0800

    Add difference with

---
+++ ThirtySecondsOfCSharp.Tests/Lists/DifferenceWithTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class DifferenceWithTest
+    {
+        [Fact]
+        public void ShouldReturnValuesOnlyInListA()
+        {
+            var a = new List<double> { 1, 1.2, 1.5, 3, 0 };
+            var b = new List<double> { 1.9, 3, 0 };
+            var difference = new List<double> { 1, 1.2 };
+
+            Assert.Equal(difference, ThirtySecondsOfCSharp.Lists.Lists.DifferenceWith(a, b, (x, y) => Math.Round(x) == Math.Round(y)));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/DifferenceWith.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> DifferenceWith<T>(List<T> a, List<T> b, Func<T, T, bool> comparator)
+        {
+            return a.FindAll(x => b.FindIndex(y => comparator(x, y)) == -1);
+        }
+    }
+}
