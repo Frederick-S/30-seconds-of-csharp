5e1ba401ff8dbb26e867ac399c82f7d7ae2d107f
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-16 19:29:20 +0800

    Add difference

---
+++ ThirtySecondsOfCSharp.Tests/Lists/DifferenceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class DifferenceTest
+    {
+        [Fact]
+        public void ShouldReturnValuesOnlyInListA()
+        {
+            var a = new List<int> { 1, 2, 3 };
+            var b = new List<int> { 1, 2, 4 };
+            var difference = new List<int> { 3 };
+
+            Assert.Equal(difference, ThirtySecondsOfCSharp.Lists.Lists.Difference(a, b));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Difference.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> Difference<T>(List<T> a, List<T> b)
+        {
+            var hashSet = new HashSet<T>(b);
+
+            return a.FindAll(x => !hashSet.Contains(x));
+        }
+    }
+}
