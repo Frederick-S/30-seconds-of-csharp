949bbe7a7b78985f3aa71fc5a08d164968beb1a7
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-16 19:50:24 +0800

    Add difference by

---
+++ ThirtySecondsOfCSharp.Tests/Lists/DifferenceByTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class DifferenceByTest
+    {
+        [Fact]
+        public void ShouldReturnValuesOnlyInListA()
+        {
+            var a = new List<double> { 2.1, 1.2 };
+            var b = new List<double> { 2.3, 3.4 };
+            var difference = new List<double> { 1.2 };
+
+            Assert.Equal(difference, ThirtySecondsOfCSharp.Lists.Lists.DifferenceBy(a, b, Math.Floor));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/DifferenceBy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> DifferenceBy<T>(List<T> a, List<T> b, Func<T, T> selector)
+        {
+            var hashSet = new HashSet<T>(b.Select(selector));
+
+            return a.FindAll(x => !hashSet.Contains(selector(x)));
+        }
+    }
+}
