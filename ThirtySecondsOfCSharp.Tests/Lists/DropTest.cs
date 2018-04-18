9162642967cc2718cc8f52ee6398b061af8b7d44
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-18 21:43:38 +0800

    Add drop

---
+++ ThirtySecondsOfCSharp.Tests/Lists/DropTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class DropTest
+    {
+        [Fact]
+        public void ShouldRemoveTwoElements()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+            var expectedList = new List<int> { 3 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.Drop(numbers, 2));
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyList()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+
+            Assert.Equal(Enumerable.Empty<int>(), ThirtySecondsOfCSharp.Lists.Lists.Drop(numbers, 42));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Drop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> Drop<T>(List<T> list, int n)
+        {
+            return list.Skip(n).ToList();
+        }
+    }
+}
