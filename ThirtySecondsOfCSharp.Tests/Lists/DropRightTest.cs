9032dfbeef54179067e07ba151ffbecf14a04bf2
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-19 19:27:54 +0800

    Add drop right

---
+++ ThirtySecondsOfCSharp.Tests/Lists/DropRightTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class DropRightTest
+    {
+        [Fact]
+        public void ShouldRemoveTwoElementsFromRight()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+            var expectedList = new List<int> { 1 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.DropRight(numbers, 2));
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyList()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+
+            Assert.Equal(Enumerable.Empty<int>(), ThirtySecondsOfCSharp.Lists.Lists.DropRight(numbers, 42));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/DropRight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> DropRight<T>(List<T> list, int n)
+        {
+            return list.Take(list.Count - n).ToList();
+        }
+    }
+}
