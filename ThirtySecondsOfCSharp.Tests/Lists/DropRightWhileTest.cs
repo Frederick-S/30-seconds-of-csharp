7594ad4fc6f961efa4c1481f0ed3dfdbee433e30
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-21 20:20:57 +0800

    Add drop right while

---
+++ ThirtySecondsOfCSharp.Tests/Lists/DropRightWhileTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class DropRightWhileTest
+    {
+        [Fact]
+        public void ShouldReturnTwoElements()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4 };
+            var expectedList = new List<int> { 1, 2 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.DropRightWhile(numbers, x => x < 3));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/DropRightWhile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> DropRightWhile<T>(List<T> list, Predicate<T> predicate)
+        {
+            return Enumerable.Range(0, list.Count)
+                .Reverse()
+                .SkipWhile(i => !predicate(list[i]))
+                .Select(i => list[i])
+                .ToList();
+        }
+    }
+}
