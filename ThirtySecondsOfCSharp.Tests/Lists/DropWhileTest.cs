1c0555b3022e091c9dbe8606436971518338e1ba
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-22 11:54:46 +0800

    Add drop while

---
+++ ThirtySecondsOfCSharp.Tests/Lists/DropWhileTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class DropWhileTest
+    {
+        [Fact]
+        public void ShouldReturnTwoElements()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4 };
+            var expectedList = new List<int> { 3, 4 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.DropWhile(numbers, x => x >= 3));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/DropWhile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> DropWhile<T>(List<T> list, Predicate<T> predicate)
+        {
+            return list.SkipWhile(x => !predicate(x))
+                .ToList();
+        }
+    }
+}
