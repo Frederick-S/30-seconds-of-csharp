83afba88baa92e1e650909b61d7c4e71c5f6d3bf
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-23 21:31:48 +0800

    Add everynth

---
+++ ThirtySecondsOfCSharp.Tests/Lists/EveryNthTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class EveryNthTest
+    {
+        [Fact]
+        public void ShouldReturnThreeElements()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var expectedList = new List<int> { 2, 4, 6 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.EveryNth(numbers, 2));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/EveryNth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> EveryNth<T>(List<T> list, int nth)
+        {
+            return Enumerable.Range(0, list.Count)
+                .Where(i => i % nth == nth - 1)
+                .Select(i => list[i])
+                .ToList();
+        }
+    }
+}
