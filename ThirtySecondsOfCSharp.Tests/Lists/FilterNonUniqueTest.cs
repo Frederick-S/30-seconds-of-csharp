52f599034886f4a41eab5c16cdc9605e70a99678
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-25 20:30:47 +0800

    Add filter non unique

---
+++ ThirtySecondsOfCSharp.Tests/Lists/FilterNonUniqueTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class FilterNonUniqueTest
+    {
+        [Fact]
+        public void ShouldReturnThreeElements()
+        {
+            var numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+            var expectedList = new List<int> { 1, 3, 5 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.FilterNonUnique(numbers));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/FilterNonUnique.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> FilterNonUnique<T>(List<T> list)
+        {
+            return list.FindAll(x => list.IndexOf(x) == list.LastIndexOf(x))
+                .ToList();
+        }
+    }
+}
