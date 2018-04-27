a10cabc26c11f53c883c307863cf1590c6da30ff
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-27 09:31:11 +0800

    Add findlast

---
+++ ThirtySecondsOfCSharp.Tests/Lists/FindLastTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class FindLastTest
+    {
+        [Fact]
+        public void ShouldReturnTheLastElementWhichPassThePredicate()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4 };
+
+            Assert.Equal(3, ThirtySecondsOfCSharp.Lists.Lists.FindLast(numbers, x => x % 2 == 1));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/FindLast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static T FindLast<T>(List<T> list, Predicate<T> predicate)
+        {
+            return list.FindLast(predicate);
+        }
+    }
+}
