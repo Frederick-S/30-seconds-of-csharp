60349dd099e0f566286f060d81e36c52d137cf60
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-27 09:45:19 +0800

    Add find last index

---
+++ ThirtySecondsOfCSharp.Tests/Lists/FindLastIndexTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class FindLastIndexTest
+    {
+        [Fact]
+        public void ShouldReturnIndexAsTwo()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4 };
+
+            Assert.Equal(2, ThirtySecondsOfCSharp.Lists.Lists.FindLastIndex(numbers, x => x % 2 == 1));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/FindLastIndex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static int FindLastIndex<T>(List<T> list, Predicate<T> predicate)
+        {
+            return list.FindLastIndex(predicate);
+        }
+    }
+}
