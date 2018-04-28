8eeecd7ec0c0709d4eb115a09d7136d435092af2
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-28 10:37:03 +0800

    Add index of all

---
+++ ThirtySecondsOfCSharp.Tests/Lists/IndexOfAllTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class IndexOfAllTest
+    {
+        [Fact]
+        public void ShouldReturnAllIndices()
+        {
+            var numbers = new List<int> { 1, 2, 3, 1, 2, 3 };
+            var expectedIndices = new List<int> { 0, 3 };
+
+            Assert.Equal(expectedIndices, ThirtySecondsOfCSharp.Lists.Lists.IndexOfAll(numbers, 1));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/IndexOfAll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<int> IndexOfAll<T>(List<T> list, T value)
+        {
+            return Enumerable.Range(0, list.Count)
+                .Where(i => list[i].Equals(value))
+                .ToList();
+        }
+    }
+}
