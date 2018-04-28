0588a4cd0e4a59caa6350208f59dde808f84690b
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-28 10:57:42 +0800

    Add initia

---
+++ ThirtySecondsOfCSharp.Tests/Lists/InitialTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class InitialTest
+    {
+        [Fact]
+        public void ShouldReturnAllElementsExceptTheLastOne()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+            var expectedList = new List<int> { 1, 2 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.Initial(numbers));
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyList()
+        {
+            Assert.Equal(new List<int>(), ThirtySecondsOfCSharp.Lists.Lists.Initial(new List<int>()));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Initial.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> Initial<T>(List<T> list)
+        {
+            return list.Count > 1 ? list.GetRange(0, list.Count - 1) : new List<T>();
+        }
+    }
+}
