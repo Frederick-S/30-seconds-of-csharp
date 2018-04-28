f0ecb544f823da3d804f99b7be16a999ce5851f6
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-28 10:26:24 +0800

    Add head

---
+++ ThirtySecondsOfCSharp.Tests/Lists/HeadTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class HeadTest
+    {
+        [Fact]
+        public void ShouldReturnTheFirstElement()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+
+            Assert.Equal(1, ThirtySecondsOfCSharp.Lists.Lists.Head(numbers));
+        }
+
+        [Fact]
+        public void ShouldThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => ThirtySecondsOfCSharp.Lists.Lists.Head(new List<int>()));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Head.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static T Head<T>(List<T> list)
+        {
+            return list[0];
+        }
+    }
+}
