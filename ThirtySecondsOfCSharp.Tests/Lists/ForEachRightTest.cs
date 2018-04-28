0e02b5ac44840068cc2febff6b87432bed65e907
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-28 09:37:51 +0800

    Add for each right

---
+++ ThirtySecondsOfCSharp.Tests/Lists/ForEachRightTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class ForEachRightTest
+    {
+        [Fact]
+        public void ShouldExecuteProvidedFunctionFromRightToLeft()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4 };
+            var actualList = new List<int>();
+            var expectedList = new List<int> { 4, 3, 2, 1 };
+
+            ThirtySecondsOfCSharp.Lists.Lists.ForEachRight(numbers, x => actualList.Add(x));
+
+            Assert.Equal(expectedList, actualList);
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/ForEachRight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static void ForEachRight<T>(List<T> list, Action<T> action)
+        {
+            Enumerable.Range(0, list.Count)
+                .Reverse()
+                .ToList()
+                .ForEach(i => action(list[i]));
+        }
+    }
+}
