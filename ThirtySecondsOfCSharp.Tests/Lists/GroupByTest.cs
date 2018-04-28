140e953ab1c5391485976576d5c6f87d3877aea1
Frederick-S <mao_xiaodan@hotmail.com>
2018-04-28 10:13:29 +0800

    Add group by

---
+++ ThirtySecondsOfCSharp.Tests/Lists/GroupByTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class GroupByTest
+    {
+        [Fact]
+        public void ShouldMapToTwoGrups()
+        {
+            var numbers = new List<double> { 6.1, 4.2, 6.3 };
+            var expectedGroups = new Dictionary<double, List<double>>
+            {
+                { 4, new List<double> { 4.2 } },
+                { 6, new List<double> { 6.1, 6.3 } },
+            };
+
+            Assert.Equal(expectedGroups, ThirtySecondsOfCSharp.Lists.Lists.GroupBy(numbers, Math.Floor));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/GroupBy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static Dictionary<TKey, List<T>> GroupBy<T, TKey>(List<T> list, Func<T, TKey> selector)
+        {
+            return list.GroupBy(selector)
+                .ToDictionary(x => x.Key, x => x.ToList());
+        }
+    }
+}
