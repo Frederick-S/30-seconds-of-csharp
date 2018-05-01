f469695a6ff65a5977de8fa54d0169fa5086fa25
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-01 13:09:10 +0800

    Add 2d array

---
+++ ThirtySecondsOfCSharp.Tests/Lists/Initialize2DArrayTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class Initialize2DArrayTest
+    {
+        [Fact]
+        public void ShouldGenerate2DArray()
+        {
+            var expectedList = new List<List<int>>
+            {
+                new List<int> { 0, 0 },
+                new List<int> { 0, 0 },
+            };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.Initialize2DArray(2, 2, 0));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Initialize2DArray.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<List<T>> Initialize2DArray<T>(int columns, int rows, T value)
+        {
+            return Enumerable.Range(1, rows)
+                .Select(i => Enumerable.Range(1, columns).Select(j => value).ToList())
+                .ToList();
+        }
+    }
+}
