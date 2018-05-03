cfdcb49c0d8d35a535bee561f0137d94e174f7a2
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-03 09:11:00 +0800

    Add initialize array with range

---
+++ ThirtySecondsOfCSharp.Tests/Lists/InitializeArrayWithRangeTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class InitializeArrayWithRangeTest
+    {
+        [Fact]
+        public void ShouldCreateNumberListFromZeroToFive()
+        {
+            var expectedList = new List<int> { 0, 1, 2, 3, 4, 5 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.InitializeArrayWithRange(5));
+        }
+
+        [Fact]
+        public void ShouldCreateNumberListFromThreeToSeven()
+        {
+            var expectedList = new List<int> { 3, 4, 5, 6, 7 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.InitializeArrayWithRange(7, 3));
+        }
+
+        [Fact]
+        public void ShouldCreateNumberListFromZeroToEightWithStepTwo()
+        {
+            var expectedList = new List<int> { 0, 2, 4, 6, 8 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.InitializeArrayWithRange(9, 0, 2));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/InitializeArrayWithRange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<int> InitializeArrayWithRange(int end, int start = 0, int step = 1)
+        {
+            return Enumerable.Range(0, (int)Math.Ceiling((end + 1.0 - start) / step))
+                .Select(i => (i * step) + start)
+                .ToList();
+        }
+    }
+}
