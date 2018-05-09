14924e8ab6392e1c692aaeeef40ba671d6243e42
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-09 10:24:11 +0800

    Add intersection with

---
+++ ThirtySecondsOfCSharp.Tests/Lists/IntersectionWithTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class IntersectionWithTest
+    {
+        [Fact]
+        public void ShouldReturnCommonElements()
+        {
+            var a = new List<double> { 1, 1.2, 1.5, 3, 0 };
+            var b = new List<double> { 1.9, 3, 0, 3.9 };
+            var expectedList = new List<double> { 1.5, 3, 0 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.IntersectionWith(a, b, (x, y) => Math.Round(x) == Math.Round(y)));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/IntersectionWith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> IntersectionWith<T>(List<T> a, List<T> b, Func<T, T, bool> comparator)
+        {
+            return a.Where(x => b.FindIndex(y => comparator(x, y)) != -1)
+                .ToList();
+        }
+    }
+}
