5d1c5441d2263c4909620bccd58af2921dfb0879
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-09 10:04:52 +0800

    Add intersection by

---
+++ ThirtySecondsOfCSharp.Tests/Lists/IntersectionByTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class IntersectionByTest
+    {
+        [Fact]
+        public void ShouldReturnCommonElements()
+        {
+            var a = new List<double> { 2.1, 1.2 };
+            var b = new List<double> { 2.3, 3.4 };
+            var expectedList = new List<double> { 2.1 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.IntersectionBy(a, b, Math.Floor));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/IntersectionBy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> IntersectionBy<T>(List<T> a, List<T> b, Func<T, T> selector)
+        {
+            var hashSet = new HashSet<T>(b.Select(selector));
+
+            return a.Where(x => hashSet.Contains(selector(x)))
+                .ToList();
+        }
+    }
+}
