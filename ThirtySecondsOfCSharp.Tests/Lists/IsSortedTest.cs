799cf0c8301191e1c672c19beb20c4c1b64986a7
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-14 10:19:21 +0800

    Add is sorted

---
+++ ThirtySecondsOfCSharp.Tests/Lists/IsSortedTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class IsSortedTest
+    {
+        [Fact]
+        public void ShouldReturnDirectionAsOne()
+        {
+            var list = new List<int> { 0, 1, 2, 2 };
+
+            Assert.Equal(1, ThirtySecondsOfCSharp.Lists.Lists.IsSorted(list));
+        }
+
+        [Fact]
+        public void ShouldReturnDirectionAsMinusOne()
+        {
+            var list = new List<int> { 4, 3, 2 };
+
+            Assert.Equal(-1, ThirtySecondsOfCSharp.Lists.Lists.IsSorted(list));
+        }
+
+        [Fact]
+        public void ShouldReturnDirectionAsZero()
+        {
+            var list = new List<int> { 4, 3, 5 };
+
+            Assert.Equal(0, ThirtySecondsOfCSharp.Lists.Lists.IsSorted(list));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/IsSorted.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static int IsSorted<T>(List<T> list)
+            where T : IComparable
+        {
+            Debug.Assert(list.Count > 1, "The list should contain at least two elements");
+
+            var direction = list[0].CompareTo(list[1]);
+
+            for (var i = 1; i < list.Count - 1; i++)
+            {
+                var result = list[i].CompareTo(list[i + 1]);
+
+                if (result != 0 && result != direction)
+                {
+                    return 0;
+                }
+            }
+
+            return -direction;
+        }
+    }
+}
