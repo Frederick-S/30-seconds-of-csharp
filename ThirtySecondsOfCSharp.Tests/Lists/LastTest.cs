05faf6140a73cfd1cd3ebbba1ac70d8c720e861a
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-14 10:56:32 +0800

    Add last

---
+++ ThirtySecondsOfCSharp.Tests/Lists/LastTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class LastTest
+    {
+        [Fact]
+        public void ShouldReturnLastElement()
+        {
+            var list = new List<int> { 1, 2, 3 };
+
+            Assert.Equal(3, ThirtySecondsOfCSharp.Lists.Lists.Last(list));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Last.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static T Last<T>(List<T> list)
+        {
+            return list.Last();
+        }
+    }
+}
