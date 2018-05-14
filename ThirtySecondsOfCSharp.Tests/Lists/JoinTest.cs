6ff7371066207fe5c4d59829cdd08f10d4fafcc6
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-14 10:46:41 +0800

    Add join

---
+++ ThirtySecondsOfCSharp.Tests/Lists/JoinTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class JoinTest
+    {
+        [Fact]
+        public void ShouldJoinListToStringWithSpecifiedSeparator()
+        {
+            var list = new List<string> { "pen", "pineapple", "apple", "pen" };
+
+            Assert.Equal("pen,pineapple,apple&pen", ThirtySecondsOfCSharp.Lists.Lists.Join(list, ",", "&"));
+        }
+
+        [Fact]
+        public void ShouldJoinListToStringWithDefaultSeparator()
+        {
+            var list = new List<string> { "pen", "pineapple", "apple", "pen" };
+
+            Assert.Equal("pen,pineapple,apple,pen", ThirtySecondsOfCSharp.Lists.Lists.Join(list));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Join.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static string Join<T>(List<T> list, string separator = ",", string endSeparator = ",")
+        {
+            if (separator == endSeparator)
+            {
+                return string.Join(separator, list);
+            }
+            else
+            {
+                return string.Join(separator, list.Take(list.Count - 1)) + string.Format("{0}{1}", endSeparator, list.Last());
+            }
+        }
+    }
+}
