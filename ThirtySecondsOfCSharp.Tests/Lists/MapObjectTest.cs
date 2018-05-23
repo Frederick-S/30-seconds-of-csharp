4d6e097f09b45f4275ef00efa4b375d7d145be94
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-23 10:22:13 +0800

    Add mapobject

---
+++ ThirtySecondsOfCSharp.Tests/Lists/MapObjectTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class MapObjectTest
+    {
+        [Fact]
+        public void ShouldReturnADictionary()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            var expectedDictionary = new Dictionary<int, int>
+            {
+                { 1, 1 },
+                { 2, 4 },
+                { 3, 9 },
+            };
+
+            Assert.Equal(expectedDictionary, ThirtySecondsOfCSharp.Lists.Lists.MapObject(list, x => x * x));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/MapObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static Dictionary<TKey, TValue> MapObject<TKey, TValue>(List<TKey> list, Func<TKey, TValue> keySelector)
+        {
+            return list.ToDictionary(x => x, x => keySelector(x));
+        }
+    }
+}
