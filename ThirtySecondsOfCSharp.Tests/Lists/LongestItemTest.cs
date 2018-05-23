d789406293ecc28d7835eee92946cf579ce45f36
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-23 09:58:14 +0800

    Add longest item

---
+++ ThirtySecondsOfCSharp.Tests/Lists/LongestItemTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class LongestItemTest
+    {
+        [Fact]
+        public void ShouldReturnTheLongestItem()
+        {
+            var lists = new List<int>[]
+            {
+                new List<int> { 1, 2, 3 },
+                new List<int> { 1, 2 },
+                new List<int> { 1, 2, 3, 4, 5 },
+            };
+            var expectedList = new List<int> { 1, 2, 3, 4, 5 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.LongestItem(lists));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/LongestItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> LongestItem<T>(params List<T>[] lists)
+        {
+            return lists.OrderBy(x => x.Count)
+                .Last();
+        }
+    }
+}
