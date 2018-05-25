33f039d962d83e68e5fe7433b83e6bc18cc5ca1c
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-25 16:23:05 +0800

    Add maxn

---
+++ ThirtySecondsOfCSharp.Tests/Lists/MaxNTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class MaxNTest
+    {
+        [Fact]
+        public void ShouldReturnThree()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            var expectedList = new List<int> { 3 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.MaxN(list));
+        }
+
+        [Fact]
+        public void ShouldReturnThreeAndTwo()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            var expectedList = new List<int> { 3, 2 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.MaxN(list, 2));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/MaxN.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> MaxN<T>(List<T> list, int n = 1)
+        {
+            return list.Select(x => x)
+                .OrderByDescending(x => x)
+                .Take(n)
+                .ToList();
+        }
+    }
+}
