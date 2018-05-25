fc0e14d423bc0ed21b27ea72f1668ca793c44e60
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-25 16:42:00 +0800

    Add minn

---
+++ ThirtySecondsOfCSharp.Tests/Lists/MinNTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class MinNTest
+    {
+        [Fact]
+        public void ShouldReturnOne()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            var expectedList = new List<int> { 1 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.MinN(list));
+        }
+
+        [Fact]
+        public void ShouldReturnOneAndTwo()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            var expectedList = new List<int> { 1, 2 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.MinN(list, 2));
+        }
+    }
+}
