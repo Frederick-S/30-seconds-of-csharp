db088677b15ece896501c4dc9dfe0b8f25e4fd48
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-28 11:38:04 +0800

    Add none

---
+++ ThirtySecondsOfCSharp.Tests/Lists/NoneTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class NoneTest
+    {
+        [Fact]
+        public void ShouldReturnTrue()
+        {
+            var list = new List<int> { 0, 1, 3, 0 };
+
+            Assert.True(ThirtySecondsOfCSharp.Lists.Lists.None(list, x => x == 2));
+        }
+
+        [Fact]
+        public void ShouldReturnFalse()
+        {
+            var list = new List<int> { 0, 1, 3, 0, 2 };
+
+            Assert.False(ThirtySecondsOfCSharp.Lists.Lists.None(list, x => x == 2));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/None.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static bool None<T>(List<T> list, Predicate<T> predicate)
+        {
+            return list.All(x => !predicate(x));
+        }
+    }
+}
