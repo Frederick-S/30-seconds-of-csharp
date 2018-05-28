96e09f1538be37794d71af56e6ce06665d624826
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-28 13:04:41 +0800

    Add nthelement

---
+++ ThirtySecondsOfCSharp.Tests/Lists/NthElementTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class NthElementTest
+    {
+        [Fact]
+        public void ShouldReturnTheSecondElement()
+        {
+            var list = new List<char> { 'a', 'b', 'c' };
+
+            Assert.Equal('b', ThirtySecondsOfCSharp.Lists.Lists.NthElement(list, 1));
+        }
+
+        [Fact]
+        public void ShouldReturnTheFirstElement()
+        {
+            var list = new List<char> { 'a', 'b', 'c' };
+
+            Assert.Equal('a', ThirtySecondsOfCSharp.Lists.Lists.NthElement(list, -3));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/NthElement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static T NthElement<T>(List<T> list, int n)
+        {
+            return n >= 0 ? list[n] : list[list.Count + n];
+        }
+    }
+}
