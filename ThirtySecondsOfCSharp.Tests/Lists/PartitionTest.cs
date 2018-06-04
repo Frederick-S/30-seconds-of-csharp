efe0ea0c319f40506d6d11be7473f435d740477d
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-04 11:53:24 +0800

    Add partition

---
+++ ThirtySecondsOfCSharp.Tests/Lists/PartitionTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class PartitionTest
+    {
+        [Fact]
+        public void ShouldGroupTheElementsIntoTwoLists()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var expectedList = new List<List<int>>
+            {
+                new List<int> { 2, 4, 6 },
+                new List<int> { 1, 3, 5 },
+            };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.Partition(list, x => x % 2 == 0));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Partition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<List<T>> Partition<T>(List<T> list, Predicate<T> predicate)
+        {
+            return list.Aggregate(new List<List<T>> { new List<T>(), new List<T>() }, (accumulator, x) =>
+            {
+                var index = predicate(x) ? 0 : 1;
+
+                accumulator[index].Add(x);
+
+                return accumulator;
+            }).ToList();
+        }
+    }
+}
