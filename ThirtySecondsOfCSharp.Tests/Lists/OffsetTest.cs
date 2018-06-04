4dbff36aafd4861771aef38c78644906e39a20c2
Frederick-S <mao_xiaodan@hotmail.com>
2018-06-04 11:10:37 +0800

    Add offset

---
+++ ThirtySecondsOfCSharp.Tests/Lists/OffsetTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class OffsetTest
+    {
+        [Fact]
+        public void ShouldMoveFirstTwoElementsToEnd()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var expectedList = new List<int> { 3, 4, 5, 1, 2 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.Offset(list, 2));
+        }
+
+        [Fact]
+        public void ShouldMoveLastTwoElementsToFront()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5 };
+            var expectedList = new List<int> { 4, 5, 1, 2, 3 };
+
+            Assert.Equal(expectedList, ThirtySecondsOfCSharp.Lists.Lists.Offset(list, -2));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/Offset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static List<T> Offset<T>(List<T> list, int n)
+        {
+            if (n >= 0)
+            {
+                return list.TakeLast(list.Count - n)
+                    .Concat(list.Take(n))
+                    .ToList();
+            }
+            else
+            {
+                return list.TakeLast(-n)
+                    .Concat(list.Take(list.Count + n))
+                    .ToList();
+            }
+        }
+    }
+}
