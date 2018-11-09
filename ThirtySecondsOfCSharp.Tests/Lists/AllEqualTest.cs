bb180a5f6b33a987edff10f51cb7e4e9f52003fc
Frederick-S <mao_xiaodan@hotmail.com>
2018-11-09 08:23:54 +0800

    Add all equal

---
+++ ThirtySecondsOfCSharp.Tests/Lists/AllEqualTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class AllEqualTest
+    {
+        [Fact]
+        public void AllNumbersAreEqual()
+        {
+            var numbers = new List<int> { 2, 2, 2, 2, 2 };
+
+            Assert.True(ThirtySecondsOfCSharp.Lists.Lists.AllEqual(numbers));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/AllEqual.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static bool AllEqual<T>(List<T> list)
+        {
+            return new HashSet<T>(list).Count <= 1;
+        }
+    }
+}
