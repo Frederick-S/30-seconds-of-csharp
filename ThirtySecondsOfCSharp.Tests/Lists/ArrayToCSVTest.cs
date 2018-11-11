47b14c5bfb0ac0addfa38449c6c727505f119928
Frederick-S <mao_xiaodan@hotmail.com>
2018-11-11 20:26:36 +0800

    Add array to csv

---
+++ ThirtySecondsOfCSharp.Tests/Lists/ArrayToCSVTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ThirtySecondsOfCSharp.Tests.Lists
+{
+    public class ArrayToCSVTest
+    {
+        [Fact]
+        public void ShouldConvertListToCSVString()
+        {
+            var list = new List<List<string>>
+            {
+                new List<string> { "a", "b" },
+                new List<string> { "c", "d" },
+            };
+
+            Assert.Equal("\"a\",\"b\"\n\"c\",\"d\"", ThirtySecondsOfCSharp.Lists.Lists.ArrayToCSV(list));
+        }
+    }
+}
---
+++ ThirtySecondsOfCSharp/Lists/ArrayToCSV.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ThirtySecondsOfCSharp.Lists
+{
+    public partial class Lists
+    {
+        public static string ArrayToCSV<T>(List<List<T>> list, string delimiter = ",")
+        {
+            return string.Join("\n", list.Select(x => string.Join(delimiter, x.Select(y => $"\"{y}\""))));
+        }
+    }
+}
